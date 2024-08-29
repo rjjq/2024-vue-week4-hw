5f5fe896a9c79580ee27c89cc42446f95149cb4b
rjjq <rrrjjjqqq@gmail.com>
2024-08-29 21:03:40 +0800

    Updates

---
+++ assets/loginStore-BVI4D_X-.js
@@ -0,0 +1,1 @@
+import{m as r,r as e}from"./index-BbjktQmv.js";const s=r("login",()=>{const o=e(""),n=e("");return{email:o,nickname:n}});export{s as u};
--- assets/loginStore-ZTVYyOv_.js
+++
@@ -1,1 +0,0 @@
-import{k as r,r as e}from"./index-BgZp6ebe.js";const s=r("login",()=>{const o=e(""),n=e("");return{email:o,nickname:n}});export{s as u};
